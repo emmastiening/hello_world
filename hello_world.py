5638b05b8a8afac2089b528a46cdbe5eaaad6979
Emma Stiening <emstiening@gmail.com>
2014-12-29 03:42:29 +0000

    hello first name last name

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,7 @@
+print "Please enter your first name:"
+first_name = raw_input()
 
-print "hello_world!"
+print "Please enter your last name:"
+last_name = raw_input() 
+
+print "hello there", first_name, last_name 
