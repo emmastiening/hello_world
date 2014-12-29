20a329b8a5171780386b65a038b47b31abe9e092
Emma Stiening <emstiening@gmail.com>
2014-12-29 03:51:55 +0000

    first and last name compliment greg's shirt

--- hello_world.py
+++ hello_world.py
@@ -2,6 +2,11 @@ print "Please enter your first name:"
 first_name = raw_input()
 
 print "Please enter your last name:"
-last_name = raw_input() 
+last_name = raw_input()
 
-print "hello there", first_name, last_name 
+print "hello there", first_name, last_name
+
+if first_name == "Greg":
+    print "Nice cat shirt, by the way."
+
+
